ad74a1bc98b10d7834d7b5c7503ba8a7fe10e386
Federico Deniard <fededeniard@gmail.com>
2024-01-11 23:12:59 -0300

    WIP Mobile version - Button fixed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ for (let i = 0; i < cards.length; i++) {
 
 // Button
 const contactButton = document.getElementsByClassName("button");
-contactButton.addEventListener("contextmenu", (e) => {
+contactButton.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 
